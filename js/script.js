796382f48dba8078b95dee4dff8497d7aae6ad72
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-08 17:05:12 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*
+---------CONSEGNA ESERCIZIO----------------
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato
+*/
+
+
+//creo l'array per l'elenco delle mail
+var elencoMail = []
+
+//creo l'elenco delle mail usando un ciclo for (da 1@gmail.com a 10@gmail.com)
+for (var i = 0; i<10;i++) {
+    if (elencoMail.length == 0) {
+        elencoMail=[i+1+'@gmail.com'];
+    }
+    else {elencoMail.push(i+1+'@gmail.com');
+    }
+}
+
+//inizializzo variabile risultato = false e chiedo di inserire una mail all'utente
+var risultato = false;
+var email = prompt('inserisci la tua mail per accedere es. 5@gmail.com');
+
+//ciclo for per verificare che la mail inserita sia presente nell'elenco delle mail generato
+for (var i=0; i < elencoMail.length; i++) {
+    if (email == elencoMail[i]) {
+        risultato = true;
+        i = elencoMail.length;
+    }
+}
+
+//resituisco risultato su HTML
+if (risultato==true) {
+    document.getElementById('risultato').innerHTML="OK la tua mail "+email+" è valida";
+    document.getElementById('risultato').style.backgroundColor="green";
+} else {
+    document.getElementById('risultato').innerHTML="KO la tua mail "+email+" non è valida";
+    document.getElementById('risultato').style.backgroundColor="red";
+}
